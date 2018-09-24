a096925d4b6199e6d5a3aac58b2c466d9e9de099
Simon Bohnen <simon.bohnen13@gmail.con>
2018-09-24 18:00:08 +0200

    heavy changes

--- experimental/connector.js
+++ experimental/connector.js
@@ -1,21 +1,45 @@
+let ctx, canv;
+let config = [
+    {sx: 400, sy: 400, ex: 600, ey: 800, diagFirst: false, iconId: "fa-stack-overflow", href: "https://stackoverflow.com/users/7804181/SimonBohnen"},
+    {sx: 1000, sy: 800, ex: 1400, ey: 600, diagFirst: false, iconId: "fa-twitter", href: "https://twitter.com/SimonBohnen"}
+    /*{sx: 400, sy: 400, ex: 400, ey: 400, diagFirst: false, iconId: "fa-stack-overflow", href: "mailto:simonbohnen@tum.de"},
+    {sx: 400, sy: 400, ex: 400, ey: 400, diagFirst: false, iconId: "fa-stack-overflow", href: "https://github.com/SimonBohnen"},
+    {sx: 400, sy: 400, ex: 400, ey: 400, diagFirst: false, iconId: "fa-stack-overflow", href: "https://xing.to/SimonBohnen"},
+    {sx: 400, sy: 400, ex: 400, ey: 400, diagFirst: false, iconId: "fa-stack-overflow", href: "https://linkedin.com/in/SimonBohnen"},
+    {sx: 400, sy: 400, ex: 400, ey: 400, diagFirst: false, iconId: "fa-stack-overflow", href: "https://www.stackoverflow.com/"},
+    {sx: 400, sy: 400, ex: 400, ey: 400, diagFirst: false, iconId: "fa-stack-overflow", href: "https://www.stackoverflow.com/"}*/
+];
+
 function draw() {
-  $("#title-wrapper").html("Simon");
-  beforeWidth = 0;
-  positionTitle();
+    $("#title-wrapper").html("Simon");
+    beforeWidth = 0;
+    positionTitle();
 
-    const c = document.querySelector("#tagCanvas");
-    const ctx = c.getContext("2d");
+    canv = document.querySelector("#tagCanvas");
+    ctx = canv.getContext("2d");
     ctx.webkitImageSmoothingEnabled = true;
-  ctx.translate(0.5, 0.5);
-  ctx.strokeStyle = '#ffffff';
-  ctx.lineCap="round";
+    //ctx.translate(0.5, 0.5);
+    ctx.strokeStyle = '#ffffff';
+    ctx.fillStyle = '#ffffff';
+    ctx.lineCap="round";
 
-  drawConnector(ctx, 400, 400, 600, 800, false);
+    drawNextConnector(0);
 
-  //drawConnector(ctx, 400, 300, 500, 700, true);
-  //drawDiagonal(ctx, 400, 200, 500, 1, 1, 0, null);
+    //drawConnector(ctx, 400, 300, 500, 700, true);
+    //drawDiagonal(ctx, 400, 200, 500, 1, 1, 0, null);
 
-  //Icons: Mail, Github, Twitter, LinkedIn, Xing
+    $(".socialIcon").css({"width": (iconRadius * 2) + "px", "height": (iconRadius * 2) + "px", "line-height": (iconRadius * 2) + "px"});
+    //Icons: Mail, Github, Twitter, LinkedIn, Xing, StackOverflow
+}
+
+function drawNextConnector(index) {
+    let curConfig = config[index];
+    drawConnector(ctx, curConfig.sx, curConfig.sy, curConfig.ex, curConfig.ey, curConfig.diagFirst, curConfig.iconId, curConfig.href);
+    if(index < config.length - 1) {
+        setTimeout(function () {
+            drawNextConnector(index + 1);
+        }, 300);
+    }
 }
 
 const iconRadius = 38;
@@ -30,131 +54,158 @@ const TL = 7;
 
 const drawspeed = 7;
 
-function drawConnector(ctx, sx, sy, ex, ey, diagFirst, iconId) {
+function drawConnector(ctx, sx, sy, ex, ey, diagFirst, iconId, href) {
     const sideways = Math.abs(ex - sx) > Math.abs(sy - ey);
     const diry = ey > sy ? 1 : -1;
     const dirx = ex > sx ? 1 : -1;
     if(diagFirst) {
-      const endDiagX = sideways ? sx + dirx * Math.abs(ey - sy) : ex;
-      drawDiagonal(ctx, sx, sy, endDiagX, dirx, diry, 0, function() {
-      if(sideways) {
-        drawHori(ctx, endDiagX, ey, ex - dirx * iconRadius, dirx, 0, function() { drawCircle(ctx, ex, ey, 0, dirx === 1 ? LEFT : RIGHT, iconId) });
-      } else {
-        drawVert(ctx, ex, sy + diry * Math.abs(ex - sx), ey - diry * iconRadius, diry, 0, function() { drawCircle(ctx, ex, ey, 0, diry === 1 ? TOP : BOTTOM, iconId) });
-      }
-    });
-  } else {
-    if(sideways) {
-        const midX = ex - dirx * Math.abs(ey - sy);
-        drawHori(ctx, sx, sy, midX, dirx, 0, function() {
-        drawDiagonal(ctx, midX, sy, ex - dirx * iconRadius * sqrt2 / 2, dirx, diry, 0, function() { drawCircle(ctx, ex, ey, 0, dirx === 1 ? (diry === 1 ? TL : BL) : (diry === 1 ? TR : BR), iconId) });
-      });
+        const endDiagX = sideways ? sx + dirx * Math.abs(ey - sy) : ex;
+        drawDiagonal(ctx, sx, sy, endDiagX, dirx, diry, 0, function() {
+            if(sideways) {
+                drawHori(ctx, endDiagX, ey, ex - dirx * iconRadius, dirx, 0, function() {
+                    drawCircle(ctx, ex, ey, 0, dirx === 1 ? LEFT : RIGHT, iconId, href);
+                });
+            } else {
+                drawVert(ctx, ex, sy + diry * Math.abs(ex - sx), ey - diry * iconRadius, diry, 0, function() {
+                    drawCircle(ctx, ex, ey, 0, diry === 1 ? TOP : BOTTOM, iconId, href);
+                });
+            }
+        });
     } else {
-        const midY = ey - diry * Math.abs(ex - sx);
-        drawVert(ctx, sx, sy, midY, diry, 0, function() {
-        drawDiagonal(ctx, sx, midY, ex - dirx * iconRadius * sqrt2 / 2, dirx, diry, 0, function() { drawCircle(ctx, ex, ey, 0, dirx === 1 ? (diry === 1 ? TL : BL) : (diry === 1 ? TR : BR), iconId) });
-      });
-      //todo vertikal, dann dia
+        if(sideways) {
+            const midX = ex - dirx * Math.abs(ey - sy);
+            drawHori(ctx, sx, sy, midX, dirx, 0, function() {
+                drawDiagonal(ctx, midX, sy, ex - dirx * iconRadius * sqrt2 / 2, dirx, diry, 0, function() {
+                    drawCircle(ctx, ex, ey, 0, dirx === 1 ? (diry === 1 ? TL : BL) : (diry === 1 ? TR : BR), iconId, href);
+                });
+            });
+        } else {
+            const midY = ey - diry * Math.abs(ex - sx);
+            drawVert(ctx, sx, sy, midY, diry, 0, function() {
+                drawDiagonal(ctx, sx, midY, ex - dirx * iconRadius * sqrt2 / 2, dirx, diry, 0, function() {
+                    drawCircle(ctx, ex, ey, 0, dirx === 1 ? (diry === 1 ? TL : BL) : (diry === 1 ? TR : BR), iconId, href);
+                });
+            });
+        }
     }
-  }
 }
 
 let circleimg;
 
-function drawCircle(ctx, x, y, prog, startFrom, callback, iconId) {
-  ctx.save();
-  if(prog === 0) {
-    circleimg = document.getElementById("circle-img");
-  }
-  ctx.translate(x, y);
-  switch(startFrom) {
-    case TOP:
-    ctx.rotate(0.5 * Math.PI);
-    break;
-    case RIGHT:
-    ctx.rotate(Math.PI);
-    break;
-    case BOTTOM:
-    ctx.rotate(1.5 * Math.PI);
-    break;
-    case TR:
-    ctx.rotate(0.75 * Math.PI);
-    break;
-    case BR:
-    ctx.rotate(1.25 * Math.PI);
-    break;
-    case BL:
-    ctx.rotate(1.75 * Math.PI);
-    break;
-    case TL:
-    ctx.rotate(0.25 * Math.PI);
-    break;
-  }
-  ctx.clearRect(-iconRadius, -iconRadius, iconRadius * 2, iconRadius * 2);
-  ctx.drawImage(circleimg, 0, 0, prog, iconRadius * 2, -iconRadius, -iconRadius, prog, iconRadius * 2);
-  ctx.restore();
-  if(prog < 2 * iconRadius) {
+function drawCircle(ctx, x, y, prog, startFrom, iconId, href) {
+    if(prog > 2 * iconRadius) {
+        showIcon(x, y, iconId, href);
+        return;
+    } else {
+        if(prog === 0) {
+            circleimg = document.getElementById("circle-img");
+        }
+        prog += drawspeed / 2.0;
+    }
+    ctx.save();
+    ctx.translate(x, y);
+    switch(startFrom) {
+        case TOP:
+            ctx.rotate(0.5 * Math.PI);
+            break;
+        case RIGHT:
+            ctx.rotate(Math.PI);
+            break;
+        case BOTTOM:
+            ctx.rotate(1.5 * Math.PI);
+            break;
+        case TR:
+            ctx.rotate(0.75 * Math.PI);
+            break;
+        case BR:
+            ctx.rotate(1.25 * Math.PI);
+            break;
+        case BL:
+            ctx.rotate(1.75 * Math.PI);
+            break;
+        case TL:
+            ctx.rotate(0.25 * Math.PI);
+            break;
+    }
+    ctx.clearRect(-iconRadius - 1, -iconRadius - 5, iconRadius * 2 + 6, iconRadius * 2 + 10);
+    ctx.drawImage(circleimg, 0, 0, prog, iconRadius * 2, -iconRadius, -iconRadius, prog, iconRadius * 2);
+    ctx.restore();
     requestAnimationFrame(function() {
-      drawCircle(ctx, x, y, prog + drawspeed / 2.0, startFrom, callback);
+        drawCircle(ctx, x, y, prog, startFrom, iconId, href);
     });
-  } else {
-    showIcon(iconId);
-  }
+}
+
+function drawCircle2(ctx, x, y, prog, angle, iconId, href) {
+    if(prog === 0) {
+        circleimg = document.getElementById("circle-img");
+    }
+    ctx.save();
+    ctx.rotate(angle);
+    ctx.clearRect(-iconRadius - 1, -iconRadius - 5, iconRadius * 2 + 6, iconRadius * 2 + 10);
+    ctx.drawImage(circleimg, 0, 0, prog, iconRadius * 2, -iconRadius, -iconRadius, prog, iconRadius * 2);
+    ctx.restore();
+    if(prog < 2 * iconRadius) {
+        requestAnimationFrame(function() {
+            drawCircle(ctx, x, y, prog + drawspeed / 2.0, angle, iconId, href);
+        });
+    } else {
+        showIcon(x, y, iconId, href);
+    }
 }
 
 const sqrt2 = Math.sqrt(2);
 
 function drawDiagonal(ctx, sx, sy, ex, dirx, diry, progx, callback) {
-  ctx.lineWidth = 1;
+    ctx.lineWidth = 1;
     const nextX = sx + dirx * progx;
     if((dirx === 1 && nextX < ex) || (dirx === -1 && nextX > ex)) {
-    ctx.clearRect(sx, sy, ex - sx, ex - sx);
-    ctx.moveTo(sx, sy);
-    ctx.lineTo(nextX, sy + diry * progx);
-    ctx.stroke();
-    requestAnimationFrame(function() {
-      drawDiagonal(ctx, sx, sy, ex, dirx, diry, progx + drawspeed / sqrt2, callback);
-    });
-  } else {
-    ctx.moveTo(sx, sy);
-    ctx.lineTo(ex, sy + diry * Math.abs(ex - sx));
-    ctx.stroke();
-    callback();
-  }
+        ctx.clearRect(sx, sy, ex - sx, ex - sx);
+        ctx.moveTo(sx, sy);
+        ctx.lineTo(nextX, sy + diry * progx);
+        ctx.stroke();
+        requestAnimationFrame(function() {
+            drawDiagonal(ctx, sx, sy, ex, dirx, diry, progx + drawspeed / sqrt2, callback);
+        });
+    } else {
+        ctx.moveTo(sx, sy);
+        ctx.lineTo(ex, sy + diry * Math.abs(ex - sx));
+        ctx.stroke();
+        callback();
+    }
 }
 
 function drawVert(ctx, sx, sy, ey, diry, progy, callback) {
-  ctx.lineWidth = 2;
+    ctx.lineWidth = 2;
     const nextY = sy + diry * progy;
     if((diry === 1 && nextY < ey) || (diry === -1 && nextY > ey)) {
-    ctx.moveTo(sx, sy);
-    ctx.lineTo(sx, nextY);
-    ctx.stroke();
-    requestAnimationFrame(function() {
-      drawVert(ctx, sx, sy, ey, diry, progy + drawspeed, callback);
-    });
-  } else {
-    ctx.moveTo(sx, sy);
-    ctx.lineTo(sx, ey);
-    ctx.stroke();
-    callback();
-  }
+        ctx.moveTo(sx, sy);
+        ctx.lineTo(sx, nextY);
+        ctx.stroke();
+        requestAnimationFrame(function() {
+            drawVert(ctx, sx, sy, ey, diry, progy + drawspeed, callback);
+        });
+    } else {
+        ctx.moveTo(sx, sy);
+        ctx.lineTo(sx, ey);
+        ctx.stroke();
+        callback();
+    }
 }
 
 function drawHori(ctx, sx, sy, ex, dirx, progx, callback) {
-  ctx.lineWidth = 2;
-  let nextX = sx + dirx * progx;
-  if((dirx === 1 && nextX < ex) || (dirx === -1 && nextX > ex)) {
-    ctx.moveTo(sx, sy);
-    ctx.lineTo(nextX, sy);
-    ctx.stroke();
-    requestAnimationFrame(function() {
-      drawHori(ctx, sx, sy, ex, dirx, progx + drawspeed, callback);
-    });
-  } else {
-    ctx.moveTo(sx, sy);
-    ctx.lineTo(ex, sy);
-    ctx.stroke();
-    callback();
-  }
+    ctx.lineWidth = 2;
+    let nextX = sx + dirx * progx;
+    if((dirx === 1 && nextX < ex) || (dirx === -1 && nextX > ex)) {
+        ctx.moveTo(sx, sy);
+        ctx.lineTo(nextX, sy);
+        ctx.stroke();
+        requestAnimationFrame(function() {
+            drawHori(ctx, sx, sy, ex, dirx, progx + drawspeed, callback);
+        });
+    } else {
+        ctx.moveTo(sx, sy);
+        ctx.lineTo(ex, sy);
+        ctx.stroke();
+        callback();
+    }
 }
--- experimental/typing.js
+++ experimental/typing.js
@@ -1,8 +1,8 @@
 WebFont.load({
-  google: {
-    families: ['Orbitron']
-  },
-  active: init
+    google: {
+        families: ['Orbitron']
+    },
+    active: init
 });
 
 let beforeWidth, simonWidth;
@@ -10,43 +10,54 @@ let beforeWidth, simonWidth;
 $(window).resize(positionTitle);
 
 function positionTitle() {
-  let headermarginleft = ($("body").width() - simonWidth) / 2 - beforeWidth;
-  $("#title-wrapper").css({"left": headermarginleft + "px"});
+    let headermarginleft = ($("body").width() - simonWidth) / 2 - beforeWidth;
+    $("#title-wrapper").css({"left": headermarginleft + "px"});
 }
 
 function init() {
-  $(function() {
-      const c = document.querySelector("#tagCanvas");
-      c.width = 0.98 * window.innerWidth;
-    c.height = 0.98 * window.innerHeight;
+    $(function() {
+        const c = document.querySelector("#tagCanvas");
+        c.width = 0.98 * window.innerWidth;
+        c.height = 0.98 * window.innerHeight;
 
-    beforeWidth = document.getElementById("measureBefore").clientWidth;
-    simonWidth = document.getElementById("measureSimon").clientWidth;
-    positionTitle();
+        beforeWidth = document.getElementById("measureBefore").clientWidth;
+        simonWidth = document.getElementById("measureSimon").clientWidth;
+        positionTitle();
 
-    drawNextCharacter(0);
-  });
+        drawNextCharacter(0);
+    });
 }
 
 const delays = [70, 70, 70, 300, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70];
 const typeString = "Hi, I'm Simon.";
 
 function drawNextCharacter(i) {
-  if (i < delays.length) {
-    $("#spell").html(typeString.substring(0, i + 1));
-    setTimeout(function() {
-      drawNextCharacter(i + 1);
-    }, delays[i]);
-  } else {
-      const leng = 300;
-      $("#flickering").animate({color: "#000"}, leng)
-                    .animate({color: "#fff"}, leng)
-                    .animate({color: "#000"}, leng)
-                    .animate({color: "#fff"}, leng)
-                    .animate({color: "#000"}, leng);
-    setTimeout(function(){
-      $("#title-wrapper").html("<span class=\"fade\">Hi, I'm </span><span>Simon</span><span class=\"fade\">.</span>");
-      $(".fade").animate({color: "#000"}, leng, "swing", draw);
-    }, 2000);
-  }
+    if (i < delays.length) {
+        $("#spell").html(typeString.substring(0, i + 1));
+        setTimeout(function() {
+            drawNextCharacter(i + 1);
+        }, delays[i]);
+    } else {
+        const leng = 300;
+        $("#flickering").animate({color: "#000"}, leng)
+            .animate({color: "#fff"}, leng)
+            .animate({color: "#000"}, leng)
+            .animate({color: "#fff"}, leng)
+            .animate({color: "#000"}, leng);
+        setTimeout(function(){
+            $("#title-wrapper").html("<span class=\"fade\">Hi, I'm </span><span>Simon</span><span class=\"fade\">.</span>");
+            $(".fade").animate({color: "#000"}, leng, "swing", waitForDraw);
+        }, 2000);
+    }
+}
+
+let count = 0;
+
+function waitForDraw() {
+    if(count === 0) {
+        count = 1;
+    } else {
+        count = 0;
+        draw();
+    }
 }
