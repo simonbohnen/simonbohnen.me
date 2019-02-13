61f9424e4304ca1f463bfadbd766b04a797cbec8
simonbohnen <simon.bohnen@tum.de>
2019-02-13 22:28:26 +0100

    rechnung

--- js/m.js
+++ js/m.js
@@ -13,8 +13,11 @@ function checkLinks() {
   });
 }
 
-var delays = [70, 70, 70, 300, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70];
-var specialPoints = {7: "<span class=\"fade\">Hi, I'm </span>", 13: "<span class=\"fade\">Hi, I'm </span>Simon<span class=\"fade\">.</span>"};
+let delays = [70, 70, 70, 300, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70];
+let specialPoints = {
+  7: "<span class=\"fade\">Hi, I'm </span>",
+  13: "<span class=\"fade\">Hi, I'm </span>Simon<span class=\"fade\">.</span>"
+};
 
 function drawNextCharacter(i) {
   if (i < delays.length) {
@@ -23,7 +26,7 @@ function drawNextCharacter(i) {
       drawNextCharacter(i + 1);
     }, delays[i]);
   } else {
-    var leng = 300;
+    let leng = 300;
     $("#flickering").animate({color: "#000"}, leng)
                     .animate({color: "#fff"}, leng)
                     .animate({color: "#000"}, leng)
