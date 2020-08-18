b85e892b14ea06a29c4818345abd545679b8ec74
Simon Bohnen <simon.bohnen@tum.de>
2020-08-18 15:27:24 +0200

    Random:

--- src/secret/experimental/js/logging.js
+++ src/secret/experimental/js/logging.js
@@ -1,6 +1,6 @@
-import $ from 'jquery';
-
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxmvfy73cZLiq50JcD17Psr4lCUjRXThgsg2l3MXQ5_6L9X5_J1/exec';
-$(() => {
-  $.getJSON(SCRIPT_URL);
-});
+// import $ from 'jquery';
+//
+// const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxmvfy73cZLiq50JcD17Psr4lCUjRXThgsg2l3MXQ5_6L9X5_J1/exec';
+// $(() => {
+//   $.getJSON(SCRIPT_URL);
+// });
